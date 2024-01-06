dd25c6d33a27f3e0fadb9706ba5712c697851538
Rafal Banaszak <rafal.banaszak.ext@nokia.com>
2024-01-06 20:37:18 +0100

    Add execution of the ExampleProject_Test to the RunExampleProject.sh

--- Scripts/RunExampleProject.sh
+++ Scripts/RunExampleProject.sh
@@ -10,16 +10,21 @@ cp ./TargetLib/slog/* ./ExampleProject/slog                                     
 cp ./TargetLib/TypeConfigs/TypeSizeDiscovery.h ./ExampleProject/slog/TypeSizeDiscovery.h                    &&\
 cp ./TargetLib/TypeConfigs/default/TypeConfig.yaml ./ExampleProject/slog                                    &&\
 # 2. Run the ProjectProcessor to process the example project
-./cmake-build-release-shrinklog-docker/ProjectProcessor ./ExampleProject                                    &&\
+./cmake-build-release-shrinklog-docker/ProjectProcessor ./ExampleProject                                             &&\
 # 3. Build the example projects
-/usr/bin/cmake --build ./cmake-build-release-shrinklog-docker -j --target                                     \
-ExampleProject_LogDemo ExampleProject_TypeSizeDiscoveryDemo ExampleProject_Test                             &&\
-# 4. Run the log demo project and save the output to logRaw.txt
-./cmake-build-release-shrinklog-docker/ExampleProject_LogDemo > ./ExampleProject/logRaw.txt                 &&\
-# 5. Decode the log.txt to logDecoded.txt
-./cmake-build-release-shrinklog-docker/LogDecoder ./ExampleProject/slog/map.slog ./ExampleProject/logRaw.txt  \
-    > ./ExampleProject/logDecoded.txt                                                                       &&\
+/usr/bin/cmake --build ./cmake-build-release-shrinklog-docker -j --target                                              \
+ExampleProject_LogDemo ExampleProject_TypeSizeDiscoveryDemo ExampleProject_Test                                      &&\
+# 4. Run the log demo project and save the output to demoRaw.txt
+./cmake-build-release-shrinklog-docker/ExampleProject_LogDemo > ./ExampleProject/demoRaw.txt                         &&\
+# 5. Decode the log.txt to demoDecoded.txt
+./cmake-build-release-shrinklog-docker/LogDecoder ./ExampleProject/slog/map.slog ./ExampleProject/demoRaw.txt         \
+    > ./ExampleProject/demoDecoded.txt                                                                               &&\
 # 6. Run the type size discovery demo
-./cmake-build-release-shrinklog-docker/ExampleProject_TypeSizeDiscoveryDemo > ./ExampleProject/typesSz.txt  &&\
+./cmake-build-release-shrinklog-docker/ExampleProject_TypeSizeDiscoveryDemo > ./ExampleProject/typesSz.txt           &&\
+# 7. Run tests
+mkdir -p ./ExampleProject/testResults
+./cmake-build-release-shrinklog-docker/ExampleProject_Test > ./ExampleProject/testResults/raw.txt                    &&\
+./cmake-build-release-shrinklog-docker/LogDecoder ./ExampleProject/slog/map.slog                                       \
+    ./ExampleProject/testResults/raw.txt > ./ExampleProject/testResults/decoded.txt                                  &&\
 echo \"SUCCESS!\"
 "
